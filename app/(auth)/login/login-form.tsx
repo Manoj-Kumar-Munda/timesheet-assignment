7b095fd2d674223d230f4ab02e04466f2859f8ed
Manoj Kumar Munda <manojkrmunda828@gmail.com>
2025-10-07 17:29:09 +0530

    Add login form and page components for user authentication

---
+++ app/(auth)/login/login-form.tsx
@@ -0,0 +1,93 @@
+"use client";
+
+import { signIn } from "next-auth/react";
+import {  useSearchParams } from "next/navigation";
+import { useState } from "react";
+
+export function LoginForm() {
+  const [email, setEmail] = useState("jsmith@example.com");
+  const [password, setPassword] = useState("123456");
+  const [error, setError] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const searchParams = useSearchParams();
+
+  const callbackUrl = searchParams.get("callbackUrl") || "/";
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setError(null);
+    setIsLoading(true);
+
+    try {
+      const result = await signIn("credentials", {
+        email,
+        password,
+        redirect: false,
+      });
+
+      if (result?.error) {
+        setError("Invalid username or password");
+        setIsLoading(false);
+      } else if (result?.ok) {
+        window.location.href = callbackUrl;
+      }
+    } catch (err) {
+      setError("An unexpected error occurred. Please try again.");
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div className="bg-white basis-1/2 flex items-center justify-center text-black">
+      <div className="max-w-xl w-full flex flex-col gap-5">
+        <h2 className="font-bold text-xl">Welcome back</h2>
+        <form className="flex flex-col gap-5" onSubmit={handleSubmit}>
+          <div className="flex flex-col gap-2">
+            <label htmlFor="email">Email</label>
+            <input
+              className="w-full border p-2 rounded-md border-gray-300 focus:outline-none focus:ring-1 focus:ring-gray-500 focus:border-gray-500 transition-all duration-300"
+              type="email"
+              id="email"
+              name="email"
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <label htmlFor="password">Password</label>
+            <input
+              className="w-full border p-2 rounded-md border-gray-300 focus:outline-none focus:ring-1 focus:ring-gray-500 focus:border-gray-500 transition-all duration-300"
+              type="password"
+              id="password"
+              name="password"
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+
+          <div className="flex items-center gap-2">
+            <input
+              type="checkbox"
+              id="remember"
+              name="remember"
+              className="w-4 h-4 rounded-md border bg-gray-300 border-gray-300 accent-blue-600"
+            />
+            <label htmlFor="remember" className="text-sm">
+              Remember me
+            </label>
+          </div>
+          {error && <p className="text-red-500">{error}</p>}
+          <button
+            type="submit"
+            className="bg-blue-600 text-white p-2 rounded-lg disabled:opacity-50"
+            disabled={isLoading}
+          >
+            {isLoading ? "Signing in..." : "Sign in"}
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,20 @@
+import { LoginForm } from "./login-form";
+
+export default function LoginPage() {
+  return (
+    <div className="min-h-screen w-full h-full flex">
+      <LoginForm />
+
+      <div className="bg-blue-600  basis-1/2  flex-col justify-center gap-3 px-10 hidden md:flex">
+        <h1 className="text-4xl font-semibold">ticktock</h1>
+        <p className="leading-normal">
+          Introducing ticktock, our cutting-edge timesheet web application
+          designed to revolutionize how you manage employee work hours. With
+          ticktock, you can effortlessly track and monitor employee attendance
+          and productivity from anywhere, anytime, using any internet-connected
+          device.
+        </p>
+      </div>
+    </div>
+  );
+}
