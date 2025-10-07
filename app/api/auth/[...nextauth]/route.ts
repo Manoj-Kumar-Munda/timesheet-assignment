6daf372a533efc9d857dbb6243988d84a109e570
Manoj Kumar Munda <manojkrmunda828@gmail.com>
2025-10-07 12:35:08 +0530

    setup next-auth

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,56 @@
+import { user } from "@/data/user";
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const handler = NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+      },
+
+      async authorize(credentials) {
+        if (!credentials?.username || !credentials?.password) {
+          throw new Error("Missing username or password");
+        }
+
+        if (
+          credentials.username === user.username &&
+          credentials.password === user.password
+        ) {
+          return user;
+        }
+
+        return null;
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60,
+  },
+  pages: {
+    signIn: "/login",
+    error: "/login",
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.id as string;
+      }
+      return session;
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+});
+
+export { handler as GET, handler as POST };
---
+++ data/user.ts
@@ -0,0 +1,7 @@
+export const user = {
+  id: "1",
+  name: "J Smith",
+  email: "jsmith@example.com",
+  username: "jsmith",
+  password: "123456",
+};
