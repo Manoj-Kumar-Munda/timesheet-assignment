6b12bc68e3f622380cc0319430c56d189b523fca
Manoj Kumar Munda <manojkrmunda828@gmail.com>
2025-10-08 09:30:40 +0530

    Update layout to include background color and flex properties for main content area

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
       >
         <Providers>
           <Header />
-          <main className="flex-1">{children}</main>
+          <main className="flex-1 bg-gray-100 flex flex-col">{children}</main>
           <Footer />
         </Providers>
       </body>
